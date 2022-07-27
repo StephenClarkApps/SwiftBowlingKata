e97f6b132e9505143c97bf317f75c3cb48365668
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2022-07-27 17:22:39 +0100

    Test   canRollGutterGame   Red

--- Tests/SwiftBowlingKataTests/BowlingKataTests.swift
+++ Tests/SwiftBowlingKataTests/BowlingKataTests.swift
@@ -7,4 +7,19 @@ final class SwiftBowlingKataTests: XCTestCase {
     func test_canCreateGame() {
         var game = BowlingGame()
     }
+    
+    func test_canRollGutterGame() {
+        var game = BowlingGame()
+        
+        // a gutter game is where a player scores nada
+        // in this context there would be no extra go
+        // so the player would have two turns per frame
+        // and there are 10 frames so 20 turns
+        
+        for i in 0...19 {
+            game.roll(0)
+        }
+        
+        XCTAssertEqual(0, game.score)
+    }
 }
